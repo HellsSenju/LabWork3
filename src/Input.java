daf9c8cc9913e6786b760a1e957ea5f724cd941d
Senju <irisha1234517@gmail.com>
2022-10-06 14:27:53 +0400

    Фул готовая, тестите плэз

--- src/Input.java
+++ src/Input.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 public class Input {
     Scanner scan = new Scanner(System.in);
     Conversion convers = new Conversion();
@@ -7,6 +6,12 @@ public class Input {
     public int finalNumSys;
     public int number;
     public String numStr;
+    public String cc11 = "0123456789A";
+    public String cc12 = "0123456789AB";
+    public String cc13 = "0123456789ABC";
+    public String cc14 = "0123456789ABCD";
+    public String cc15 = "0123456789ABCDE";
+    public String cc16 = "0123456789ABCDF";
     public void InputOrigin() {
         while (true) {
             System.out.print("Выберите исходную систему счисления: 2 - 16: -->");
@@ -36,9 +41,7 @@ public class Input {
         }
     }
     public void InputNumber(){
-        //TODO добавить проверку на ввод
         boolean minus = false;
-        boolean run;
         while(true){
             System.out.print("Введите число: -->");
             if(originalNumSys < 11){
@@ -60,49 +63,17 @@ public class Input {
             }
             else{
                 try{
-                    //TODO если не создавать новый сканер, то он первый рза не берет строку, а второй раз берет
                     Scanner scanf = new Scanner(System.in);
-                    run = true;
                     numStr = scanf.nextLine();
                     numStr = numStr.toUpperCase();
-                    int[] arr = new int[numStr.length()];
-                    if((int)numStr.charAt(0) == 45) minus = true;
-                    for(int i = 0; i < numStr.length(); i++){
-                        if( ((int)numStr.charAt(i)  >= 65 && (int)numStr.charAt(i)  <= 70) ){
-                            switch((int)numStr.charAt(i) ){
-                                case 65:
-                                    arr[i] = 10;
-                                    break;
-                                case 66:
-                                    arr[i] = 11;
-                                    break;
-                                case 67:
-                                    arr[i] = 12;
-                                    break;
-                                case 68:
-                                    arr[i] = 13;
-                                    break;
-                                case 69:
-                                    arr[i] = 14;
-                                    break;
-                                case 70:
-                                    arr[i] = 15;
-                                    break;
-                            }
-                        }
-                        else if( ((int)numStr.charAt(i) >= 48 && (int)numStr.charAt(i) <= 57) ){
-                            arr[i] = Integer.parseInt(String.valueOf(numStr.charAt(i)));
-                        }
-                        else{
-                            System.out.println("Недоступное значение числа");
-                            run = false;
-                            break;
-                        }
-                    }
-                    if(run){
+                    if(Check()) {
+                        int[] arr = new int[numStr.length()];
+                        if ((int) numStr.charAt(0) == 45) minus = true;
+                        arr = addToString(arr);
                         convers.Convers(originalNumSys, finalNumSys, 0, arr, minus);
                         break;
                     }
+                    System.out.println("Неверный символ");
                 }catch (Exception ex){
                     System.out.println();
                     System.out.println(ex.getMessage());
@@ -110,5 +81,62 @@ public class Input {
                 }
             }
         }
+    }
+
+    public int[] addToString(int[] arr){
+        for(int i = 0; i < numStr.length(); i++){
+            if( ((int)numStr.charAt(i)  >= 65 && (int)numStr.charAt(i)  <= 70) ){
+                switch((int)numStr.charAt(i) ){
+                    case 65:
+                        arr[i] = 10;
+                        break;
+                    case 66:
+                        arr[i] = 11;
+                        break;
+                    case 67:
+                        arr[i] = 12;
+                        break;
+                    case 68:
+                        arr[i] = 13;
+                        break;
+                    case 69:
+                        arr[i] = 14;
+                        break;
+                    case 70:
+                        arr[i] = 15;
+                        break;
+                }
+            }
+            else
+                arr[i] = Integer.parseInt(String.valueOf(numStr.charAt(i)));
+        }
+        return arr;
+    }
+
+    public boolean Check(){
+        for(int i = 0; i < numStr.length(); i++){
+            String temp = Character.toString(numStr.charAt(i));
+            switch(originalNumSys){
+                case 11:
+                    if(cc11.contains(temp)) return true;
+                    break;
+                case 12:
+                    if(cc12.contains(temp)) return true;
+                    break;
+                case 13:
+                    if(cc13.contains(temp)) return true;
+                    break;
+                case 14:
+                    if(cc14.contains(temp)) return true;
+                    break;
+                case 15:
+                    if(cc15.contains(temp)) return true;
+                    break;
+                case 16:
+                    if(cc16.contains(temp)) return true;
+                    break;
+            }
+        }
+        return false;
     }
 }
